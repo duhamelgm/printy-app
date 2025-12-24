d7972682ea74231d5c57e4588261ba1432692110
Duhamel Guerrero <duhamelgm@gmail.com>
2025-12-24 00:16:55 -0500

    Remove ssl requirement

--- backend/printing_queue.py
+++ backend/printing_queue.py
@@ -1,7 +1,7 @@
 from redis import Redis
 import os
 
-redis_client = Redis.from_url(os.getenv("REDIS_URL"))
+redis_client = Redis.from_url(os.getenv("REDIS_URL"), ssl_cert_reqs=None)
 
 def enqueue_print(print_id: int) -> None:
   redis_client.lpush("printing_queue", print_id)
